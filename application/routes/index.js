7db52bfcd828057cde6da3cf32fe0baa65a6a5cb
chl <eaclickbait@gmail.com>
2023-06-03 20:35:37 -0700

    foo test

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
+
+router.get('/foo', (req,res,next)=>{
+	res.send('caught')
+})
 
 module.exports = router;
